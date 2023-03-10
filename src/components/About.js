cb1ea5a2e82d3cfb5e1d9b27db6f79cd70250fd1
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-03-10 09:07:26 +0000

    Adding images  and working on object positioning in About component

--- src/components/About.js
+++ src/components/About.js
@@ -33,7 +33,7 @@ function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="./me2.jpeg"
           />
         </div>
       </div>
