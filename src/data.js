b1c8903f00656c1071c886b94a810fac535588c2
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-03-14 15:43:43 +0000

    Worked on Projects component and added data file for project

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: "Bi-Linguist",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./Bilinguist.png",
+    link: "https://bilinguist.netlify.app",
+  },
+  {
+    title: "OnlyPets",
+    subtitle: "Express, Node, Mongo DB",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./OnlyPets.png",
+    link: "https://only-pets.fly.dev",
+  },
+  {
+    title: "Space Doggo",
+    subtitle: "P5, JS, HTML, CSS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./SpaceDoggo.png",
+    link: "https://jaime-laureano.github.io/SpaceDoggo/",
+  },
+];
