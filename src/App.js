2ec7071b5657a7138dd8cc430abdf271825f80ae
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-03-20 19:09:25 +0000

    Adding and Working on Testimonials component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Projects />
       <Skills />
+      <Testimonials />
       <Contact />
       <Footer />
     </main>
