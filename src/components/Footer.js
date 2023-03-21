72daa7277f5281b709a471b8a52c0bee9cd255fe
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-03-21 10:57:57 +0000

    Continued styling of Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,38 +2,36 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class=" bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-      <div class="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" class="hover:underline">
-            Flowbite™
+    <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-3 dark:bg-gray-800 dark:border-gray-600">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2023{" "}
+        <a href="https://github.com/Jaime-Laureano" class="hover:underline">
+          Jaime Laureano
+        </a>
+        . All Rights Reserved.
+      </span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#about" class="mr-4 hover:underline md:mr-6 ">
+            About
           </a>
-          . All Rights Reserved.
-        </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-          <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
-              Licensing
-            </a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
+        </li>
+        <li>
+          <a href="#projects" class="mr-4 hover:underline md:mr-6">
+            Past Work
+          </a>
+        </li>
+        <li>
+          <a href="#skills" class="mr-4 hover:underline md:mr-6">
+            Skills
+          </a>
+        </li>
+        <li>
+          <a href="#testimonials" class="hover:underline">
+            Testimonials
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
