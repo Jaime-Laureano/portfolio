18cf872ffec1282ea5b5a9add3039b2cf34b6e48
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-03-28 19:14:02 +0100

    edited footer to include github and linkedin icons and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,17 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class="md:sticky bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-3 dark:bg-gray-800 dark:border-gray-600">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2023{" "}
-        <a href="https://github.com/Jaime-Laureano" class="hover:underline">
-          Jaime Laureano
+    <footer class="md:sticky bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-2 dark:bg-gray-800 dark:border-gray-600">
+      <div className="flex flex-wrap items-center">
+        <a href="https://www.youtube.com/watch?v=je98UffdN18" className="">
+          <img className=" w-7 h-7 ml-5" alt="lime" src="./lime3.png" />
         </a>
-        . All Rights Reserved.
-      </span>
-      <div className="flex justify-center">
-        <img className=" w-5 h-5" alt="lime" src="./lime3.png" />
+        <a href="https://github.com/Jaime-Laureano" className="">
+          <img className=" w-7 h-7 ml-7" alt="github" src="./github.png" />
+        </a>{" "}
+        <a href="https://www.linkedin.com/in/jaime-laureano/" className="">
+          <img className=" w-7 h-7 ml-7" alt="linkedin" src="./linkedin.png" />
+        </a>
       </div>
       <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
@@ -30,7 +31,7 @@ function Footer() {
           </a>
         </li>
         <li>
-          <a href="#testimonials" class="hover:underline">
+          <a href="#testimonials" class="hover:underline mr-4">
             References
           </a>
         </li>
