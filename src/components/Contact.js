f309a3a4ff67114f9910a38894a5b9031595f480
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-03-29 17:26:17 +0100

    still working on 404 error withregards to netlify form. Updated text on site to advise this

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,28 @@
 import React from "react";
 
 export default function Contact() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap mb-2">
@@ -38,17 +60,17 @@ export default function Contact() {
           </div>
         </div>
         <form
-          data-netlify="true"
+          netlify
           name="contact"
-          method="POST"
+          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Let's Work Together!
+            UNDER CONSTRUCTION
           </h2>
           <p className="leading-relaxed mb-5">
-            Please feel free to get in touch with me with regards to anything at
-            all. I would love to hear from you!
+            Currently fixing a few deployment bugs with regards to this form.
+            Will hopefully have this up and running soon!
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
